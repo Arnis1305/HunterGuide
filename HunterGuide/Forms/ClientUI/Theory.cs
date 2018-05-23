dd083e56164f489277f24d37cee7cffae0274f63
Arnis1305 <31897595+Arnis1305@users.noreply.github.com>
2018-05-24 00:27:59 +0300

    PDF reader and message sender

---
+++ HunterGuide/Forms/ClientUI/Theory.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using iTextSharp.text.pdf.parser;
+using MetroFramework.Forms;
+
+namespace HunterGuide.Forms.ClientUI
+{
+    public partial class Theory : MetroForm
+    {
+        
+        public Theory()
+        {
+            InitializeComponent();
+        }
+
+        private void Closebutton1_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+
+        private void OpenButton_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog() { Filter = "PDF files|*.pdf", ValidateNames = true, Multiselect = false })
+            {
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    try
+                    {
+                        iTextSharp.text.pdf.PdfReader reader = new iTextSharp.text.pdf.PdfReader(ofd.FileName);
+                        StringBuilder sb = new StringBuilder();
+                        for(int i =1; i<=reader.NumberOfPages; i++)
+                        {
+                            sb.Append(PdfTextExtractor.GetTextFromPage(reader, i));
+                        }
+                        richTextBox.Text = sb.ToString();
+                        reader.Close();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+        }
+    }
+}
